f147df006de3923e68e0a78d8399246d947e932b
Raúl Angel Jiménez <Raulangel51@users.noreply.github.com>
2020-05-18 13:08:48 -0600

    f

---
+++ JimenezRaul_Laboratorio8/Assets/Scripts/Alarm.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alarm : MonoBehaviour
+{
+    Light L;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        L = other.GetComponent<Light>();
+        L.intensity = Mathf.Lerp(1,2, 2 * Time.deltaTime);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        L = other.GetComponent<Light>();
+        L.intensity = 0;
+    }
+}
--- JimenezRaul_Laboratorio8/Assets/Scripts/FlashLight.cs
+++ JimenezRaul_Laboratorio8/Assets/Scripts/FlashLight.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class FlashLight : MonoBehaviour
 {
-    bool active = true;
+    bool act = true;
+    public AudioClip boton;
     public Light light;
+    private AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -18,10 +20,22 @@ public class FlashLight : MonoBehaviour
     {
         if(Input.GetMouseButtonDown(0))
         {
-            active = !active;
-            this.gameObject.SetActive(active);
-            //light.enabled = active;
-            
+            //active = !active;
+            //this.gameObject.SetActive(active);
+            //audioSource.PlayOneShot(boton);
+            //gameObject.SetActive(!gameObject.activeInHierarchy);
+            if (act == true)
+            {
+                this.gameObject.SetActive(false);
+                audioSource.PlayOneShot(boton);
+                act = false;
+            }
+            if (act == false)
+            {
+                this.gameObject.SetActive(true);
+                audioSource.PlayOneShot(boton);
+                act = true;
+            }
         }
     }
 }
