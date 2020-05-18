ef6286feb3922cac95ae8026ab35961edbadd79f
Raúl Angel Jiménez <Raulangel51@users.noreply.github.com>
2020-05-18 14:03:18 -0600

    flashlight

--- JimenezRaul_Laboratorio8/Assets/Scripts/FlashLight.cs
+++ JimenezRaul_Laboratorio8/Assets/Scripts/FlashLight.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class FlashLight : MonoBehaviour
 {
-    bool act = true;
+    //bool act = true;
     public AudioClip boton;
     public Light light;
     private AudioSource audioSource;
@@ -20,22 +20,32 @@ public class FlashLight : MonoBehaviour
     {
         if(Input.GetMouseButtonDown(0))
         {
+            if(Input.GetMouseButtonDown(0) && light.enabled == true)
+            {
+                audioSource.PlayOneShot(boton);
+                light.enabled = false;
+            }
+            else if(Input.GetMouseButtonDown(0) && light.enabled == false)
+            {
+                audioSource.PlayOneShot(boton);
+                light.enabled = true;
+            }
             //active = !active;
             //this.gameObject.SetActive(active);
             //audioSource.PlayOneShot(boton);
             //gameObject.SetActive(!gameObject.activeInHierarchy);
-            if (act == true)
-            {
-                this.gameObject.SetActive(false);
-                audioSource.PlayOneShot(boton);
-                act = false;
-            }
-            if (act == false)
-            {
-                this.gameObject.SetActive(true);
-                audioSource.PlayOneShot(boton);
-                act = true;
-            }
+            //if (act == true)
+            //{
+            //    this.gameObject.SetActive(false);
+            //    audioSource.PlayOneShot(boton);
+            //    act = false;
+            //}
+            //if (act == false)
+            //{
+            //    this.gameObject.SetActive(true);
+            //    audioSource.PlayOneShot(boton);
+            //    act = true;
+            //}
         }
     }
 }
